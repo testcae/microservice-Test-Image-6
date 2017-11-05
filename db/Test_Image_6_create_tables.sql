da0a54d3e4b90122115bbf84842f786934eca3be
ukimiawz <deathmoon123>
2017-11-05 21:27:03 +0100

    Generated microservice version 1.0

---
+++ db/Test_Image_6_create_tables.sql
@@ -0,0 +1,17 @@
+--
+-- Database Schema:  dbMusic
+-- Automatically generated sql script for the service Test Image 6, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table tblImage.
+--
+CREATE TABLE dbMusic.tblImage (
+  path VARCHAR(255) ,
+  id VARCHAR(255) ,
+CONSTRAINT id_PK PRIMARY KEY (id)
+ 
+);
+
+
+
